15a71c8485e3914e733ae6b9f18cedd104a66619
Leanne <leanne_porter@hotmail.com>
2021-08-12 15:25:15 +1000

    starting code

--- UFO - Level 1/static/js/app.js
+++ UFO - Level 1/static/js/app.js
@@ -11,15 +11,40 @@ function buildTable(data) {
     
         const row = tbody.append("tr");
 
-
-        Object.values(dataRow).forEach((val) => {
-            row.append("td").text(val);
+        Object.values(dataRow).forEach((value) => {
+            row.append("td").text(value);
         }
         );
     });
 }
 
 buildTable(tableData);
+
+var filterButton = d3.select("#filter-btn");
+
+filterButton.on("click", runFilter)
+
+function runFilter() {
+
+    d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    // console.log(inputValue);
+    // console.log(tableData);
+
+    var filteredData = tableData.filter(dataEntry => dataEntry.datetime === inputValue);
+
+    console.log(filteredData);
+
+    tbody.html("");
+
+    tbody.append("tr").filteredData;
+
+
+}
 
 
 
