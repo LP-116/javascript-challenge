bf5ca58100ec487c08746a44d2c38f70fc2e6b65
Leanne <leanne_porter@hotmail.com>
2021-08-12 12:22:31 +1000

    starting code

--- UFO - Level 1/static/js/app.js
+++ UFO - Level 1/static/js/app.js
@@ -2,3 +2,24 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+
+    data.forEach((dataRow) => {
+    
+        const row = tbody.append("tr");
+
+
+        Object.values(dataRow).forEach((val) => {
+            row.append("td").text(val);
+        }
+        );
+    });
+}
+
+buildTable(tableData);
+
+
+
