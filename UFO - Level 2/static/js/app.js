61c734b75da8662519b8aa115f15faac05687d34
Leanne <leanne_porter@hotmail.com>
2021-08-14 12:16:35 +1000

    updating code

--- UFO - Level 2/static/js/app.js
+++ UFO - Level 2/static/js/app.js
@@ -2,3 +2,88 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+
+    data.forEach((dataRow) => {
+    
+        var row = tbody.append("tr");
+
+        Object.values(dataRow).forEach((value) => {
+            row.append("td").text(value);
+        }
+        );
+    });
+}
+
+buildTable(tableData);
+
+var filterButton = d3.select("#filter-btn");
+
+filterButton.on("click", runFilter)
+
+function runFilter() {
+
+    d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    // console.log(inputValue);
+    // console.log(tableData);
+
+    // if inputValue 
+
+    var filteredData = tableData.filter(dataEntry => dataEntry.datetime === inputValue);
+
+    console.log(filteredData);
+
+    if (!filteredData.length) {
+        console.log("No result")
+
+        tbody.html("");
+
+        tbody.text("No results for selected date");
+
+    }
+
+    else {
+    
+    tbody.html("");
+
+    filteredData.forEach((dataEntry) => {
+
+        var row = tbody.append("tr");
+
+        Object.values(dataEntry).forEach((value) => {
+            row.append("td").text(value);
+        });
+    });
+};
+};
+
+var filterButton2 =  d3.select("#filter-btn2");
+
+filterButton2.on("click", resetFilter);
+
+function resetFilter() {
+
+    tbody.html("");
+
+    // var inputElement = d3.select("#datetime");
+
+    // var inputValue = inputElement.property("value");
+
+    // inputValu;
+
+    document.querySelector("#datetime").value = '';
+
+    buildTable(tableData);
+
+};
+
+
+
