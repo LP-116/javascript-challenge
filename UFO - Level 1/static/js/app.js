f7c4d7404bdc7e65feccd6886e914dca242d771b
Leanne <leanne_porter@hotmail.com>
2021-08-17 11:41:03 +1000

    updating code

--- UFO - Level 1/static/js/app.js
+++ UFO - Level 1/static/js/app.js
@@ -1,8 +1,10 @@
 // from data.js
 var tableData = data;
 
+// d3 to select the body of the html page.
 var tbody = d3.select("tbody");
 
+// creating a function to build the table containing all the data.
 function buildTable(data) {
 
     data.forEach((dataRow) => {
@@ -16,11 +18,22 @@ function buildTable(data) {
     });
 }
 
+// running the build table function.
 buildTable(tableData);
 
+
+// Now using d3 to select the filter button and on the click of the filter button, the run filter function is executed.
 var filterButton = d3.select("#filter-btn");
 
 filterButton.on("click", runFilter)
+
+
+// The runfilter function firstly prevents the page reloading.
+// Next the datetime element is selected and the input value within the datetime element is determined.
+// The filter is then completed based on the inputvalue. 
+// If the input value matches a date in the table, that data is returned.
+// If no date is entered into the field, the table displays all results.
+// If the date entered does not appear in the table, text containing "No results for selected date." is returned.
 
 function runFilter() {
 
@@ -57,6 +70,10 @@ function runFilter() {
     });
 };
 };
+
+
+// To give the user the chance to reset the table, a reset data button was created.
+// This button when clicked, clears the table results and reruns the buildtable function. 
 
 var filterButton2 =  d3.select("#filter-btn2");
 
